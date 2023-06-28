7755e313fff5f26de1c8e0f9322688eaf31792c1
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-06-28 03:53:29 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const vegetables = ingredients.map(name => {
+  const li = document.createElement('li');
+li.classList.add('item');
+li.textContent = name;
+return li;
+})
+list.append(...vegetables)
+
+
+
