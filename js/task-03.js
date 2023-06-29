7f2ae05fa7b0ac72611c032f19773012756e2de2
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-06-29 15:31:31 +0300

    task-3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector('.gallery');
+const markup = images.map(({ url, alt }) => `
+<li>
+      <img src="${url}" alt="${alt}" width = "300"/>
+    </li>
+`).join('');
+list.insertAdjacentHTML("beforeend", markup);
+console.log(markup);
