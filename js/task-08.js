87a5459615152e677b9b82f7d06860e161ca2c31
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-02 01:50:30 +0300

    task8-almost

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(evt) {
+    evt.preventDefault();
+
+    const { email, password } = evt.currentTarget.elements;
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(data);
+// Доробити alert та reset
+};
